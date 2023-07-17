63e869a793ad85ae42c38532fbf1804f92f92119
Arno Pan <apan11@my.bcit.ca>
2023-07-17 16:33:44 -0700

    Initial Commit

---
+++ SwiftUI-Car-LocalDB/SwiftUI_Car_LocalDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Car_LocalDBApp.swift
+//  SwiftUI-Car-LocalDB
+//
+//  Created by Arno Pan on 2023-07-17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Car_LocalDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
